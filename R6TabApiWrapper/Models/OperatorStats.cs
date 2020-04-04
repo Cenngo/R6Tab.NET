09eecd25d40b2bb7e10fca79697e25ba271547cb
Cenk Ergen <cenk.ergen1@gmail.com>
2020-04-05 00:07:41 +0300

    Added Functionality to OperatorData Class

---
+++ R6TabApiWrapper/Models/OperatorStats.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace R6Api.Models
+{
+	public class OperatorStats
+	{
+		public int Wins { get; internal set; }
+		public int Losses { get; internal set; }
+		public int Kills { get; internal set; }
+		public int Deaths { get; internal set; }
+		public int TimePlayed { get; internal set; }
+	}
+}
